c70d18c63c4d6efa092d3b7a823659e56ecabac4
rudensergey <rudensergey@gmail.com>
2020-02-11 14:09:31 +0300

    first success test // a videorate speed was increased to 3x times

--- popup.js
+++ popup.js
@@ -1,4 +1,14 @@
 let increaseButton = document.getElementById("increaseRate");
-increaseButton.onclick(() => {
-   document.querySelector("video").playbackRate = 2.8;
-});
+
+increaseButton.onclick = function() {
+   console.log("it works");
+   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+         code: 'document.querySelector("video").playbackRate = 3;'
+      });
+   });
+};
+
+// function increaseRate() {
+//     document.querySelector("video").playbackRate = 3;
+// }
