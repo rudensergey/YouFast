f088d182d0f7ccad9193faa2d29bc653c056a6e5
rudensergey <rudensergey@gmail.com>
2020-02-11 13:27:20 +0300

    Fix problem with default images

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.runtime.onInstalled.addListener(function() {
-   alert("Extention was installed!");
+   console.log("Extention was installed!");
 });
---
+++ popup.js
@@ -0,0 +1,4 @@
+let increaseButton = document.getElementById("increaseRate");
+increaseButton.onclick(() => {
+   document.querySelector("video").playbackRate = 2.8;
+});
