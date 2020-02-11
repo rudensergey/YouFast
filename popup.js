9f2cbcf473a388085a52e81f200590528f82d787
rudensergey <rudensergey@gmail.com>
2020-02-12 02:32:01 +0300

    rewrite all function and added them in object

--- popup.js
+++ popup.js
@@ -1,53 +1,46 @@
-let increaseButton = document.getElementById("increaseRate");
-let defaultButton = document.getElementById("defaultRate");
-let decreaseButton = document.getElementById("decreaseRate");
-let current = document.querySelector(".current");
+document.getElementById("increaseRate").addEventListener("click", function() {
+   controller.changeRate("increase");
+});
 
-console.log(current);
+document.getElementById("decreaseRate").addEventListener("click", function() {
+   controller.changeRate("decrease");
+});
 
-let currentRate = 1;
+document.getElementById("defaultRate").addEventListener("click", function() {
+   controller.changeRate("default");
+});
 
-increaseButton.onclick = function() {
-   if (currentRate <= 3) {
-      let newRate = currentRate + 0.5;
+let controller = {
+   defaultValue: 1,
+   currentValue: 1,
+   output: document.querySelector(".current"),
 
+   changeRate: function(type) {
+      switch (type) {
+         case "increase":
+            this.currentValue <= 3
+               ? this.inject(this.currentValue + 0.5)
+               : alert("Maximum rate");
+            break;
+         case "decrease":
+            this.currentValue > 0.5
+               ? this.inject(this.currentValue - 0.5)
+               : alert("Sooooo slow. It's limit");
+            break;
+         case "default":
+            this.inject(this.defaultValue);
+            break;
+      }
+   },
+
+   inject: function(newRate) {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
          chrome.tabs.executeScript(tabs[0].id, {
             code: `document.querySelector("video").playbackRate = ${newRate};`
          });
       });
 
-      currentRate = newRate;
-      current.textContent = currentRate;
-   } else {
-      alert("Maximum rate");
-   }
-};
-
-defaultButton.onclick = function() {
-   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.executeScript(tabs[0].id, {
-         code: 'document.querySelector("video").playbackRate = 1;'
-      });
-   });
-
-   currentRate = 1;
-   current.textContent = currentRate;
-};
-
-decreaseButton.onclick = function() {
-   if (currentRate > 0.5) {
-      let newRate = currentRate - 0.5;
-
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-         chrome.tabs.executeScript(tabs[0].id, {
-            code: `document.querySelector("video").playbackRate = ${newRate};`
-         });
-      });
-
-      currentRate = newRate;
-      current.textContent = currentRate;
-   } else {
-      alert("Sooooo slow. It's limit");
+      this.output.textContent = newRate;
+      this.currentValue = newRate;
    }
 };
