d7fd03b3f3e140032c2809742fc7d25a594d7e10
rudensergey <rudensergey@gmail.com>
2020-02-11 15:16:44 +0300

    create new logo // start to style

--- popup.js
+++ popup.js
@@ -1,14 +1,42 @@
 let increaseButton = document.getElementById("increaseRate");
+let defaultButton = document.getElementById("defaultRate");
+let decreaseButton = document.getElementById("decreaseRate");
+let current = document.getElementById("currentRate");
+
+let currentRate = 1;
 
 increaseButton.onclick = function() {
-   console.log("it works");
+   if (currentRate === 3.5) alert("Maximum rate");
+   let newRate = currentRate + 0.5;
+
    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
-         code: 'document.querySelector("video").playbackRate = 3;'
+         code: `document.querySelector("video").playbackRate = ${newRate};`
       });
    });
+
+   currentRate = newRate;
 };
 
-// function increaseRate() {
-//     document.querySelector("video").playbackRate = 3;
-// }
+defaultButton.onclick = function() {
+   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+         code: 'document.querySelector("video").playbackRate = 1;'
+      });
+   });
+
+   currentRate = 1;
+};
+
+decreaseButton.onclick = function() {
+   if (currentRate === 0.5) alert("Sooooo slow");
+   let newRate = currentRate - 0.5;
+
+   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+         code: 'document.querySelector("video").playbackRate = 0.5;'
+      });
+   });
+
+   currentRate = newRate;
+};
