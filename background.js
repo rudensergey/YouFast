355feef8b8bf16c8874d94967229042468afa42b
rudensergey <rudensergey@gmail.com>
2020-02-12 15:51:56 +0300

    add new shortcuts to manifest and edit background.js

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
-chrome.runtime.onInstalled.addListener(function() {
-   console.log("Extention was installed!");
+chrome.commands.onCommand.addListener(function(command) {
+   if (command === "increase_rate") {
+      console.log("inc");
+   } else if (command === "decrease_rate") {
+      console.log("dec");
+   } else if (command === "default_rate") {
+      console.log("def");
+   }
 });
--- popup.js
+++ popup.js
@@ -10,6 +10,19 @@ document.getElementById("defaultRate").addEventListener("click", function() {
    controller.changeRate("default");
 });
 
+chrome.commands.onCommand.addListener(function(command) {
+   if (command === "increase_rate") {
+      console.log("1");
+      controller.changeRate("decrease");
+   } else if (command === "decrease_rate") {
+      console.log("1");
+      controller.changeRate("increase");
+   } else if (command === "default_rate") {
+      console.log("1");
+      controller.changeRate("default");
+   }
+});
+
 let controller = {
    defaultValue: 1,
    currentValue: 1,
